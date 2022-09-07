e4238d31b9d2ad91464ffc22dec779b9ecb26386
dmadon <deanna.madon@gmail.com>
2022-09-06 21:24:36 -0500

    begin work on profile view

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,13 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 const approvalQueueRoutes = require('./approval-queue-routes');
+const profileRoutes = require('./profile-routes');
 
 router.use('/approval-queue',approvalQueueRoutes);
 router.use('/dashboard',dashboardRoutes);
 router.use('/api',apiRoutes);
 router.use('/',homeRoutes);
+router.use('/profile',profileRoutes);
 
 // This is so that if we make a request to any endpoint that doesn't exist,
 // we will receive a 404 error indicating we have requested an incorrect resource:
