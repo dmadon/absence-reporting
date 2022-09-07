3f3a78cc11c4b14164e3047b4ed60918f44b6aff
dmadon <deanna.madon@gmail.com>
2022-09-07 13:36:23 -0500

    approvers can see count of upcoming absences for each employee on profile page

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const {User, Department, Absence} = require('../models');
 const withAuth = require('../utils/auth');
+const sequelize = require('../config/connection');
+
+// THESE ARE THE '/profile' ROUTES:
+
+const today = new Date();
+
+// GET THE LOGGED-IN EMPLOYEE'S PROFILE INFO:
 
 router.get('/', withAuth,(req,res) => {
     User.findOne({
@@ -20,25 +27,27 @@ router.get('/', withAuth,(req,res) => {
             {
                 model:User,
                 as:'employees',
-                attributes:['id','first_name','last_name'],
-                order:['last_name'],
+                attributes:['id','first_name','last_name',
+                    [sequelize.literal(`(SELECT COUNT(*) FROM absence WHERE absence.user_id = employees.id)`),'total_absences'],
+                    [sequelize.literal(`(SELECT COUNT(*) FROM absence WHERE absence.user_id = employees.id AND absence.end_date > CURRENT_DATE AND absence.status = 'Approved')`),'upcoming_absences']],
                 include:{
                     model:Absence,
                     attributes:['id','start_date','end_date','absence_hours','leave_type_id','status','created_at','updated_at'],
                 }
-                
             }
-        ]
+        ],
+        order:[['employees','last_name','ASC']]
     })
     .then(dbUserData => {
         const profile = dbUserData.get({plain:true});
         const employees = dbUserData.employees.map(employee => employee.get({plain:true}))
+        // const sort = employees.sort(function(a,b){let x=a.last_name.toLowerCase();let y=b.last_name.toLowerCase();if(x<y){return-1;}if(x>y){return 1;}return 0;})
         const user_id = req.session.user_id;
         const username = req.session.username;
         const is_approver = req.session.is_approver;
         const loggedIn = req.session.loggedIn;
         res.render('profile', {profile,employees,user_id,username,is_approver,loggedIn})     
-        // res.json(employees)
+
     })
     .catch(err => {
         console.log(err);
