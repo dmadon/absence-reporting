8af5b9fbe0baa105baf198cfb08f0d86149dd758
Chris Key <robertschris010@gmail.com>
2022-09-06 20:56:59 -0500

    updating profile routes

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -11,8 +11,36 @@ router.get('/:id', (req,res) => {
         attributes:{exclude:['password']},
         include:[
             {
-                
+                model:User,
+                as:'approver',
+                attributes:['id', 'first_name', 'last_name']
+            },
+            {
+                model:User,
+                as:'employees',
+                attributes:['id','first_name','last_name'],
+                include:{
+                    model:Absence,
+                    attributes:['id','start_date','end_date','absence_hours','leave_type_id','status','created_at','updated_at'],
+                }
             }
         ]
     })
+    .then(dbUserData => {
+        if(!dbUserData){
+            res.status(404).json({message: 'No user found with given id'});
+            return;
+        }
+        else{
+            res.render(dbUserData)
+        }
+    })
+
+
+
+
+
+
+
+
 })
